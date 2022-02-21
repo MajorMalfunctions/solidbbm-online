814c563550a3108b0c133597253ee22975b76f70
jaybee-aicpa <jaybee.aicpa@gmail.com>
2022-02-22 01:23:46 +0800

    fix updates

--- app/controllers/user/Post.controller.js
+++ app/controllers/user/Post.controller.js
@@ -24,48 +24,63 @@ exports.uploadFile =  async (req, res) => {
   await fs.readFile(file.path, function (err, data) {
         if (err) throw err; // Something went wrong!
         console.log(err)
-        
-        const params = {
-            Bucket: BUCKET_NAME,
-            Key: file.filename, 
-             Body: data
-        }   
+        console.log(file)
+       let a = {
+           
+        pathName: file.filename,
+        fileName: file.originalname,
+        // url: data.Location,
+        fileType: type,
+        fileFormat: file.mimetype
+
+       }
+
+        return  res.status(200).json(a)
+
+
+
+
+        // const params = {
+        //     Bucket: BUCKET_NAME,
+        //     Key: file.filename, 
+        //      Body: data
+        // }   
         
 
-        s3.upload(params, function (err, data) {
-                // Whether there is an error or not, delete the temp file
+        // s3.upload(params, function (err, data) {
+        //         // Whether there is an error or not, delete the temp file
                 
-                fs.unlink(file.path, function (err) {
-                    if (err) {
-                        console.error(err);
-                    }
-                    console.log("Deleted File");
-                });
+        //         fs.unlink(file.path, function (err) {
+        //             if (err) {
+        //                 console.error(err);
+        //             }
+        //             console.log("Deleted File");
+        //         });
 
-                console.log("PRINT FILE:", file);
-                console.log("PRINT DATA FILE:", data);
+        //         console.log("PRINT FILE:", file);
+        //         console.log("PRINT DATA FILE:", data);
 
-                if (err) {
-                    console.log('ERROR MSG: ', err);
-                    res.status(500).send(err);
-                } else {
-                    Media.create({
-                        pathName: file.filename,
-                        fileName: file.originalname,
-                        url: data.Location,
-                        fileType: type,
-                        fileFormat: file.mimetype
-                    })
-                    .then(a => {
-                        console.log(a)
-                        return  res.status(200).json(a)
-                    })
-                    .catch(err => {
-                        console.log(err)
-                        return res.status(400).json({ message: { text: 'Something went wrong!', type: 'error'}});
-                    })
-                }
-            });
+        //         if (err) {
+        //             console.log('ERROR MSG: ', err);
+        //             res.status(500).send(err);
+        //         } else {
+        //             Media.create({
+        //                 pathName: file.filename,
+        //                 fileName: file.originalname,
+        //                 url: data.Location,
+        //                 fileType: type,
+        //                 fileFormat: file.mimetype
+        //             })
+        //             .then(a => {
+        //                 console.log(a)
+        //                 return  res.status(200).json(a)
+        //             })
+        //             .catch(err => {
+        //                 console.log(err)
+        //                 return res.status(400).json({ message: { text: 'Something went wrong!', type: 'error'}});
+        //             })
+        //         }
+        //     });
         });
 }
 
